ea50b3b168f7d55d4aa0a31a2371ff15772fba3a
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-11-12 10:28:34 +0530

    Add files via upload

---
+++ MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+  image:  Pepperoni, 
+  price: "Rs.2500.00",
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price:"Rs.2780.00",
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    price: "Rs.2250.00",
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: "Rs.1950.00",
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price:"Rs.1850.00",
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: "Rs.3680.00",
+  },
+];
